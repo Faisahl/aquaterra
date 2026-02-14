750bf03410465ec0925eed3ab89c6275eb9aeed1
Faisal Bokhari <Faisbokh@gmail.com>
2026-02-14 22:34:26 +0500

    build changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://faisahl.github.io/aquaterra/',
+  base: '/aquaterra/',
+});
